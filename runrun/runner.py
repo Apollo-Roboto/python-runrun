549099622c04d22c2e5ee4f1c87aa2a274968487
Apollo-Roboto <41392253+Apollo-Roboto@users.noreply.github.com>
2023-11-02 23:26:04 -0400

    added runner

--- runrun/runner.py
+++ runrun/runner.py
@@ -1,4 +1,22 @@
+import sys
 
+from runrun.models import Command
+from runrun.command_parser import CommandParser
+from runrun.exceptions import CLIException, DefaultExceptionHandler, BaseExceptionHandler
 
 class Runner:
-    pass
+	def __init__(self,
+		command: Command,
+		exception_handler: BaseExceptionHandler = DefaultExceptionHandler(),
+	):
+		self.command = command
+		self.exception_handler = exception_handler
+
+	def run(self, args: list[str] | None = None):
+		if args is None:
+			args = sys.argv[1:]
+		try:
+			cmd = CommandParser(self.command).parse(args)
+			cmd.run()
+		except CLIException as e:
+			self.exception_handler.handle_exception(e)
